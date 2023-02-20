7f5e56e3b6f1ccbf41943313ad22af5bc9412718
dsatyap <dsatyap@amazon.com>
2023-02-20 02:09:43 +0000

    change web text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn codecatalyst/i);
   expect(linkElement).toBeInTheDocument();
 });
